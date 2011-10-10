61b65d10663dab602d0496391b53c2c4778095e0
GetAssista <kozlovkk@gmail.com>
2011-10-10 05:23:40 +0000

    quick fix
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1066 629c617a-e569-4347-8ce9-cac2fd893f29

--- ModuleSystem/module_items.py
+++ ModuleSystem/module_items.py
@@ -1200,7 +1200,7 @@ items =[
 ["ent_head_helm3","Ent_Head",[("ent_head3",0)],itp_type_head_armor|itp_unique,0,1,weight(250)|head_armor(60)|difficulty(70),0],
 ["ent_feet_boots","Ent_Feet",[("ent_foot",0)],itp_type_foot_armor|itp_unique,0,1,weight(250)|head_armor(0)|body_armor(0)|leg_armor(60)|difficulty(70),0],
 ["ent_hands","Ent_Hands",[("ent_hand_L",0)],itp_type_hand_armor|itp_unique,0,1,weight(250)|body_armor(1)|difficulty(70),0],
-["galadriel","Galadriel_suit",[("galadriel",0)],itp_type_body_armor|itp_covers_legs|itp_unique,0,1,weight(225)|head_armor(0)|body_armor(0)|leg_armor(0)|difficulty(0),0,],
+["galadriel","Galadriel_suit",[("galadriel",0)],itp_type_body_armor|itp_covers_legs|itp_unique|itp_civilian,0,1,weight(225)|head_armor(60)|body_armor(80)|leg_armor(60)|difficulty(0),0,],
 ["empty_hands","empty_hands",[("0",0)],itp_type_hand_armor|itp_unique,0,130,weight(225)|body_armor(1)|difficulty(0),0],
 ["empty_legs","empty_legs",[("0",0)],itp_type_foot_armor|itp_unique,0,130,weight(225)|leg_armor(1)|difficulty(0),0],
 ["empty_head","empty head",[("0",0)],itp_type_head_armor|itp_unique|itp_covers_head,0,1,weight(250)|head_armor(50)|difficulty(0),0],
