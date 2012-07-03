93d5cb34092728b5490c9fdf74b20c21d5e69608
CppCoder <rsr2792@gmail.com>
2012-07-03 15:46:58 +0000

    git-svn-id: https://xp-dev.com/svn/TLD_GA@1599 629c617a-e569-4347-8ce9-cac2fd893f29

---
+++ ModuleSystem/module_mission_templates_morale.py
@@ -0,0 +1,9 @@
+from header_common import *
+from header_operations import *
+from header_mission_templates import *
+from header_animations import *
+from header_sounds import *
+from header_music import *
+from module_constants import *
+
+
