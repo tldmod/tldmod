44073342bcc0cd3e0b5def420a267a2189634a49
swyter <swyterzone@gmail.com>
2013-08-23 17:45:06 +0000

    *refactor a bit the build scripts so now:  -restarts the compilation on key press  -doesn't spits compiled pyc files  -we've less redundancy
    ---
    first commit, yay!
    thanks to merlkir and getassista for the swiftness
    
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1701 629c617a-e569-4347-8ce9-cac2fd893f29

--- ModuleSystem/build_module_check_tags.bat
+++ ModuleSystem/build_module_check_tags.bat
@@ -1,37 +1,42 @@
-@echo off
-python process_init.py
-python process_global_variables.py
-python process_strings.py
-python process_skills.py
-python process_music.py
-python process_animations.py
-python process_meshes.py
-python process_sounds.py
-python process_skins.py
-python process_map_icons.py
-python process_factions.py
-python process_items.py
-python process_scenes.py
-python process_troops.py
-python process_particle_sys.py
-python process_scene_props.py
-python process_tableau_materials.py
-python process_presentations.py
-python process_party_tmps.py
-python process_parties.py
-python process_quests.py
-python process_info_pages.py
-python process_scripts.py
-python process_mission_tmps.py
-python process_game_menus.py
-python process_simple_triggers.py
-python process_dialogs.py
-python process_global_variables_unused.py
-python process_postfx.py
-@del *.pyc
+@echo off && title building tld for 1.011--
+set PATH="C:\Python24";"C:\Python26";%PATH%
+
+:top
+cls
+
+python -B -OO process_init.py
+python -B -OO process_global_variables.py
+python -B -OO process_strings.py
+python -B -OO process_skills.py
+python -B -OO process_music.py
+python -B -OO process_animations.py
+python -B -OO process_meshes.py
+python -B -OO process_sounds.py
+python -B -OO process_skins.py
+python -B -OO process_map_icons.py
+python -B -OO process_factions.py
+python -B -OO process_items.py
+python -B -OO process_scenes.py
+python -B -OO process_troops.py
+python -B -OO process_particle_sys.py
+python -B -OO process_scene_props.py
+python -B -OO process_tableau_materials.py
+python -B -OO process_presentations.py
+python -B -OO process_party_tmps.py
+python -B -OO process_parties.py
+python -B -OO process_quests.py
+python -B -OO process_info_pages.py
+python -B -OO process_scripts.py
+python -B -OO process_mission_tmps.py
+python -B -OO process_game_menus.py
+python -B -OO process_simple_triggers.py
+python -B -OO process_dialogs.py
+python -B -OO process_global_variables_unused.py
+python -B -OO process_postfx.py
+:: @del *.pyc -- not needed anymore
 echo.
 echo ______________________________
 echo.
 echo Script processing has ended.
-echo Press any key to exit. . .
-pause>nul
+echo Press any key to restart. . .
+pause>nul && goto :top
