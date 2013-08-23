3753dc561cea303f5af414a273eda6465ec8f001
swyter <swyterzone@gmail.com>
2013-08-23 20:32:52 +0000

    *first wb compat revision, which adds the following stuff:   -coded a handy symlinking script to avoid redundancy    and keep everything tidy, separated and nice.   -keep the different opcode files separated in the build, and working   -set the default wb building location in module_info
     [!] right now one can only compile in wb by changing the switch
         in module_info, this is only a temporary measure.
    
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1704 629c617a-e569-4347-8ce9-cac2fd893f29

--- ModuleSystem/module_info.py
+++ ModuleSystem/module_info.py
@@ -2,8 +2,10 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 from header_common import *
-  
+
+wb_compile_switch = 0
+
 if (wb_compile_switch == 0):
   export_dir = "../"
 elif (wb_compile_switch == 1):
-  export_dir = "C:/Games/WB/Modules/TLD/"
+  export_dir = "../_wb/"
---
+++ _wb/_setupsymlinks.bat
@@ -0,0 +1,33 @@
+@echo off && title creating wb symlinks-- && goto :start
+
+:createlink
+ echo Creating junction for ^<%2^>...
+ mklink /%1 "%2" "../%2"
+ goto :eof
+
+:start
+call :createlink J Data
+call :createlink J Languages
+call :createlink J Music
+call :createlink J Resource
+call :createlink J SceneObj
+call :createlink J Sounds
+call :createlink J Textures
+
+call :createlink H main.bmp
+call :createlink H module.ini
+
+
+echo. 
+echo Now comes the fun part. Enter the path to the warband folder.
+echo. 
+echo [!] A junction is just a shortcut which acts like a real folder,
+echo     so don't create a module folder manually under WB, it's automatic!
+echo. 
+echo     Close to skip, in case it^'s already done.
+echo. 
+set /p wbpath=Type something like [c:\wb\modules\tld] : 
+mklink /J %wbpath% .
+
+echo. && echo --- && echo It^'s done, you can close this window now... How cool is that?
+                     echo Oh, and compile Warband to generate the text files! && pause>nul
