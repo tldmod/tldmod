33bc39eac4465a98bcc1013f60f9b83e497fc761
swyter <swyterzone@gmail.com>
2013-08-23 18:22:55 +0000

    *restructure the msys tree a bit, moving the header,  id and process files to their own subfolders.
     much tidier now. and works just as before.
    
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1702 629c617a-e569-4347-8ce9-cac2fd893f29

--- ModuleSystem/build_module_check_tags.bat
+++ ModuleSystem/build_module_check_tags.bat
@@ -1,38 +1,39 @@
 @echo off && title building tld for 1.011--
 set PATH="C:\Python24";"C:\Python26";%PATH%
+set PYTHONPATH=%cd%;%cd%\data;%cd%\header;%cd%\id;%cd%\process
 
 :top
 cls
 
-python -B -OO process_init.py
-python -B -OO process_global_variables.py
-python -B -OO process_strings.py
-python -B -OO process_skills.py
-python -B -OO process_music.py
-python -B -OO process_animations.py
-python -B -OO process_meshes.py
-python -B -OO process_sounds.py
-python -B -OO process_skins.py
-python -B -OO process_map_icons.py
-python -B -OO process_factions.py
-python -B -OO process_items.py
-python -B -OO process_scenes.py
-python -B -OO process_troops.py
-python -B -OO process_particle_sys.py
-python -B -OO process_scene_props.py
-python -B -OO process_tableau_materials.py
-python -B -OO process_presentations.py
-python -B -OO process_party_tmps.py
-python -B -OO process_parties.py
-python -B -OO process_quests.py
-python -B -OO process_info_pages.py
-python -B -OO process_scripts.py
-python -B -OO process_mission_tmps.py
-python -B -OO process_game_menus.py
-python -B -OO process_simple_triggers.py
-python -B -OO process_dialogs.py
-python -B -OO process_global_variables_unused.py
-python -B -OO process_postfx.py
+python -B -OO process\process_init.py
+python -B -OO process\process_global_variables.py
+python -B -OO process\process_strings.py
+python -B -OO process\process_skills.py
+python -B -OO process\process_music.py
+python -B -OO process\process_animations.py
+python -B -OO process\process_meshes.py
+python -B -OO process\process_sounds.py
+python -B -OO process\process_skins.py
+python -B -OO process\process_map_icons.py
+python -B -OO process\process_factions.py
+python -B -OO process\process_items.py
+python -B -OO process\process_scenes.py
+python -B -OO process\process_troops.py
+python -B -OO process\process_particle_sys.py
+python -B -OO process\process_scene_props.py
+python -B -OO process\process_tableau_materials.py
+python -B -OO process\process_presentations.py
+python -B -OO process\process_party_tmps.py
+python -B -OO process\process_parties.py
+python -B -OO process\process_quests.py
+python -B -OO process\process_info_pages.py
+python -B -OO process\process_scripts.py
+python -B -OO process\process_mission_tmps.py
+python -B -OO process\process_game_menus.py
+python -B -OO process\process_simple_triggers.py
+python -B -OO process\process_dialogs.py
+python -B -OO process\process_global_variables_unused.py
+python -B -OO process\process_postfx.py
 :: @del *.pyc -- not needed anymore
 echo.
 echo ______________________________
