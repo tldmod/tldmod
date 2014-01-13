89565569b894044d5a3b328f49268d4d68b09233
swyter <swyterzone@gmail.com>
2014-01-13 11:26:31 +0000

    *raise the shader compiler priority, still slow as hell! dynamic workflow, please! *make waterfalls transparent, enable alpha blending in its material, did it work before? how?
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1740 629c617a-e569-4347-8ce9-cac2fd893f29

--- _wb/_compile_fx64.cmd
+++ _wb/_compile_fx64.cmd
@@ -1,6 +1,6 @@
 @echo off
 :start
-fxc64 /D /nologo /D PS_2_X=ps_2_b /Tfx_2_0 /Fomb.fx mb_src.fx
+start /high /b fxc64 /D /nologo /D PS_2_X=ps_2_b /Tfx_2_0 /Fomb.fx mb_src.fx
 
 
 echo Shader processing has ended.
