03ab7cebee72bf81a1cda526d5167817f97822bd
swyter <swyterzone@gmail.com>
2014-01-18 09:32:22 +0000

    *tentative fix for all the scene entering crashes, works for me (tm). test and comment.
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1771 629c617a-e569-4347-8ce9-cac2fd893f29

--- ModuleSystem/Process/process_scenes.py
+++ ModuleSystem/Process/process_scenes.py
@@ -47,6 +47,42 @@ def save_scenes(variables,variable_uses,tag_uses):
   ofile.write("scenesfile version 1\n")
   ofile.write(" %d\n"%len(scenes))
   for scene in scenes:
+  
+    #swy-- force to mark as enabled the disable_grass flag on Warband to avoid crashes
+    #      a terrain code is made of five chunks of eight hex numbers each (32 bits)
+    #      
+    #      disable_grass is enabled when there's a b01 within its two bit bracket, located at 2 bit lshifts in the 5th chunk.    
+    #      reference: http://mbmodwiki.ollclan.eu/SceneObj#Terrain_Codes
+    
+    from module_info import wb_compile_switch as is_a_wb_scene
+    
+    if (is_a_wb_scene):
+      
+      # enable the flag
+      terrain_code = int(scene[7],16) | 0b01 << (32 * 5) + 2
+      
+      # 0x000000012c60280000034cd300003efe00004b34000059be
+      #        0x52c60280000034cd300003efe00004b34000059beL
+      #          ^
+      
+      # convert tuple to list, so we can modify it
+      scene = list(scene)
+      
+      # format it as a string w/ zero padding until the 48th hex number!
+      scene[7] = '0x%048x\n' % terrain_code
+      
+      # go back to being an immutable tuple because we can
+      scene = tuple(scene)
+      
+      
+      # the result is something like this, before/after.
+      # doesn't looks like much, but fixes all the crashes.
+      
+      # 0x0000000329602800000691a400003efe00004b34000059be
+      # 0x0000000729602800000691a400003efe00004b34000059be
+      #          ^
+      
+    
     ofile.write("scn_%s %s %d %s %s %s %s %s %s %s %s "%(convert_to_identifier(scene[0]),replace_spaces(scene[0]),scene[1], scene[2],scene[3],sf(scene[4][0]),sf(scene[4][1]),sf(scene[5][0]),sf(scene[5][1]),sf(scene[6]),scene[7]))
     passages = scene[passages_pos]
     ofile.write("\n  %d "%len(passages))
