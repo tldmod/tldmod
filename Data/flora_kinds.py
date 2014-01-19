54f70d93d7ddc78fb15acbc33745d860e915c6f2
swyter <swyterzone@gmail.com>
2014-01-19 17:06:56 +0000

    *implement flag unpacking, almost there!
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1777 629c617a-e569-4347-8ce9-cac2fd893f29

--- Data/flora_kinds.py
+++ Data/flora_kinds.py
@@ -2,6 +2,33 @@
 
 result = []
 
+tflags = [
+  #terrain condition flags -- lifted from wb's module_data thing!
+  ["fkf_plain"            , 0x00000004],
+  ["fkf_steppe"           , 0x00000008],
+  ["fkf_snow"             , 0x00000010],
+  ["fkf_desert"           , 0x00000020],
+  ["fkf_plain_forest"     , 0x00000400],
+  ["fkf_steppe_forest"    , 0x00000800],
+  ["fkf_snow_forest"      , 0x00001000],
+  ["fkf_desert_forest"    , 0x00002000],
+  ["fkf_terrain_mask"     , 0x0000ffff],
+
+  ["fkf_realtime_ligting" , 0x00010000],  #deprecated
+  ["fkf_point_up"         , 0x00020000],  #uses auto-generated point-up(quad) geometry for the flora kind
+  ["fkf_align_with_ground", 0x00040000],  #align the flora object with the ground normal
+  ["fkf_grass"            , 0x00080000],  #is grass
+  ["fkf_on_green_ground"  , 0x00100000],  #populate this flora on green ground
+  ["fkf_rock"             , 0x00200000],  #is rock 
+  ["fkf_tree"             , 0x00400000],  #is tree -> note that if you set this parameter, you should pass additional alternative tree definitions
+  ["fkf_snowy"            , 0x00800000],
+  ["fkf_guarantee"        , 0x01000000],
+
+  ["fkf_speedtree"        , 0x02000000],  #NOT FUNCTIONAL: we have removed speedtree support on M&B Warband
+
+  ["fkf_has_colony_props" , 0x04000000],  # if fkf_has_colony_props -> then you can define colony_radius and colony_treshold of the flora kind
+]
+
 with open("flora_kinds.txt") as f:
     for line in f:
         result.append(line.split())
@@ -13,13 +40,22 @@ print("there are %u plants in there!\n"%nflora)
 
 for n in range(0,nflora):
   cur_line = result[nlines]
-  #print(cur_line)
   
   thiid=str(cur_line[0])
   flags=int(cur_line[1])
   nmesh=int(cur_line[2])
   
-  print("\tid: %s flags: 0x%x nmeshes: %u"%(thiid, flags, nmesh))
+  
+  dflag = []
+  
+  for x in tflags:
+    if flags & x[1]==x[1]:
+      #print(x)
+      dflag.append(x[0])
+      
+  dflag = "|".join(dflag)
+  
+  print("\tid: %s flags: 0x%x dflag: %s nmeshes: %u"%(thiid, flags, dflag, nmesh))
   
   nlines+=1
   
