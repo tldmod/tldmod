88e8255f7a68c9a8be173dd34057aef7250d1d6a
swyter <swyterzone@gmail.com>
2014-04-01 11:16:39 +0000

    *first stab at fixing up the info_page thingies. with postprocess to ease it up.
    git-svn-id: https://xp-dev.com/svn/TLD_GA@1972 629c617a-e569-4347-8ce9-cac2fd893f29

--- ModuleSystem/ID/ID_info_pages.py
+++ ModuleSystem/ID/ID_info_pages.py
@@ -1,8 +1,8 @@
-ip_morale = 0
+ip_formations_and_morale = 0
 ip_economy = 1
-ip_courtship = 2
-ip_politics = 3
-ip_character_backgrounds = 4
-ip_military_campaigns = 5
+ip_war_of_the_ring = 2
+ip_starting_tips = 3
+ip_rumours_and_legendary_places = 4
+ip_trolls = 5
 
 
