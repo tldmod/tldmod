aa13a8234f37dde4dd53b1f2ac014c141350c98d
swyter <swyterzone@gmail.com>
2014-10-03 14:21:25 +0000

    *compile the 1.011 version with the latest changes, see if they work fine (confirmed), recompile shaders too. *make a copy of the new compiled shader replacing the one inside of the `Data` folder... just for historical reasons,  if someone still follows the old instructions will still work.
    git-svn-id: https://xp-dev.com/svn/TLD_GA@2034 629c617a-e569-4347-8ce9-cac2fd893f29

--- _wb/_compile_fx64_mb1011.cmd
+++ _wb/_compile_fx64_mb1011.cmd
@@ -3,6 +3,9 @@
 @echo off && title compiling the tld hlsl shaders thing-- && echo ^[working hard...^] ^[started at %time%^] && echo. 
 start /b /wait /i /high /realtime fxc64 /D /nologo /D PS_2_X=ps_2_a /Tfx_2_0 /Fo..\mb.fx ..\mb_src.fx
 
+:: make a copy of the new compiled shader replacing the one inside of the `Data` folder... just for historical reasons, if someone still follows the old instructions will still work.
+copy ..\mb.fx ..\data\mb.fxo
+
 echo. && echo Shader processing has ended at %time%.
 echo Press any key to recompile. . .
 echo ___________________________________
