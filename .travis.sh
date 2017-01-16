3f548908277f2fd52d1d2e031b427983cdab46b7
Swyter <swyterzone@gmail.com>
2017-01-16 18:11:18 +0100

    *make the build script prettier.

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,13 @@
-sudo apt-get install p7zip tree git
-git fetch --unshallow
+# swy: as seen here <https://github.com/travis-ci/travis-ci/issues/2285> but made prettier with custom wrappers
+_fold_start_() { echo -en "travis_fold:start:script.$(( ++fold_count ))\\r" && echo -ne '\033[1;33m' && echo $1 && echo -ne '\e[0m'; }
+_fold_final_() { echo -en "travis_fold:end:script.$fold_count\\r"; }
+
+_fold_start_ '[Turning original shallow clone into a full one and installing dependencies]'
+    sudo apt-get install p7zip tree git
+    git fetch --unshallow
+_fold_final_
 
 echo HI THERE! && SVNREV=$(git rev-list --count HEAD)
-
-# swy: as seen here <https://github.com/travis-ci/travis-ci/issues/2285> but made prettier with custom wrappers
-_fold_start_() { echo -en "travis_fold:start:script.$(( ++fold_count ))\\r" && echo -e '\033[1;33m' && echo $1 && echo -e '\033[1;m'; }
-_fold_final_() { echo -en "travis_fold:end:script.$fold_count\\r"; }
 
 _fold_start_ '[Initial TLD tree view]'
     tree -h .
@@ -13,12 +15,14 @@ _fold_final_
 
 cd ModuleSystem
 
-_fold_start_ "Compiling retail revision $SVNREV!"
+_fold_start_ "[Compiling retail revision $SVNREV!]"
     curl https://ccrma.stanford.edu/~craig/utility/flip/flip.cpp -O -J && sudo g++ flip.cpp -o /usr/bin/flip
 
+    # disable cheat mode for the generated nightly builds...
     sed -i 's/cheat_switch = 1/cheat_switch = 0/' module_constants.py
 
-    title() { true; } && export -f title
+    # add a placeholder 'title' program to avoid log spam...
+    echo '#/usr/bin/bash' > /usr/bin/title && chmod +x /usr/bin/title
 
     ./build_module.sh
     ./build_module_wb.sh
@@ -28,7 +32,7 @@ _fold_final_
 
 cd ..
 
-_fold_start_ "Packaging revision $SVNREV!"
+_fold_start_ "[Packaging revision $SVNREV!]"
     git config --global core.quotepath false
     git diff --name-status --diff-filter=ACMRTUXB TLD3.3REL ./ > diff.txt
 
@@ -52,7 +56,7 @@ _fold_start_ "Packaging revision $SVNREV!"
     cd ../TLD
     #tree -h .
 
-    #fixed Linux case-sensitive language files detection
+    # fixed Linux case-sensitive language files detection
     mv Languages languages
 
     cd ..
@@ -66,15 +70,15 @@ _fold_start_ "Packaging revision $SVNREV!"
     # overwrite the content in the warband version with the files from the _wb subfolder
     cp -rf TLD_WB/_wb/* TLD_WB/
 
-    #remove the now empty _wb subfolder from the warband version
+    # remove the now empty _wb subfolder from the warband version
     rm -rf TLD_WB/_wb
     rm -f  TLD_WB/Data/mb.fxo
 
-    #paste the original optimized warband glsl shaders in GLShadersOptimized
+    # paste the original optimized warband glsl shaders in GLShadersOptimized
     curl https://github.com/tldmod/tldmod/releases/download/TLD3.3REL/vanilla_glsl_opt.zip -L -O
     unzip vanilla_glsl_opt.zip -d ./TLD_WB
 
-    #move our custom tld shaders into their rightful place
+    # move our custom tld shaders into their rightful place
     mv TLD_WB/GLShaders/*.glsl TLD_WB/GLShadersOptimized/
 
     #tree -h .
@@ -98,11 +102,11 @@ PS: For more info and official support/updates take a look to <https://tldmod.gi
     cp notice "THIS IS AN AUTOMATED RELEASE OF TLD FOR WARBAND, REVISION $SVNREV"
 _fold_final_
 
-_fold_start_ 'Compressing finished TLD packages...'
+_fold_start_ '[Compressing finished TLD packages...]'
     7zr a -mx9 -r -y $bbfile TLD "THIS IS AN AUTOMATED RELEASE OF TLD FOR M&B 1.011, REVISION $SVNREV"
 
-    rm -rf TLD
-    mv TLD_WB TLD
+    # swap the M&B 1.011 folder by the Warband one
+    rm -rf TLD && mv TLD_WB TLD
 
     7zr a -mx9 -r -y $bbfilewb TLD "THIS IS AN AUTOMATED RELEASE OF TLD FOR WARBAND, REVISION $SVNREV"
 _fold_final_
@@ -111,7 +115,7 @@ _fold_start_ '[Final tree view]'
     ls -ra
 _fold_final_
 
-_fold_start_ 'Uploading finished TLD packages...'
+_fold_start_ '[Uploading finished TLD packages...]'
     curl https://bitbucket.org/Swyter/bitbucket-curl-upload-to-repo-downloads/raw/default/upload-to-bitbucket.sh -O -J && chmod +x ./upload-to-bitbucket.sh
 
     sh ./upload-to-bitbucket.sh $bbuser $bbpass $bbpage "$bbfile"
