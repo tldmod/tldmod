86fa1fcd33481f1ff71c518c04f15b0edded22e8
Swyter <swyterzone@gmail.com>
2017-05-07 21:46:32 +0200

    *that seemed to work. do the same thing with the Windows batch files. boom! ultrafast!

--- ModuleSystem/build_module.bat
+++ ModuleSystem/build_module.bat
@@ -5,35 +5,37 @@ set PYTHONPATH=%cd%;%cd%\data;%cd%\header;%cd%\id;%cd%\process
 :top
 cls
 
-python -B -OO process\process_init.py
-python -B -OO process\process_global_variables.py
-python -B -OO process\process_strings.py
-python -B -OO process\process_skills.py
-python -B -OO process\process_music.py
-python -B -OO process\process_animations.py
-python -B -OO process\process_meshes.py
-python -B -OO process\process_sounds.py
-python -B -OO process\process_skins.py
-python -B -OO process\process_map_icons.py
-python -B -OO process\process_factions.py
-python -B -OO process\process_items.py
-python -B -OO process\process_scenes.py
-python -B -OO process\process_troops.py
-python -B -OO process\process_particle_sys.py
-python -B -OO process\process_scene_props.py
-python -B -OO process\process_tableau_materials.py
-python -B -OO process\process_presentations.py
-python -B -OO process\process_party_tmps.py
-python -B -OO process\process_parties.py
-python -B -OO process\process_quests.py
-python -B -OO process\process_info_pages.py && rem <-- just for wb
-python -B -OO process\process_scripts.py
-python -B -OO process\process_mission_tmps.py
-python -B -OO process\process_game_menus.py
-python -B -OO process\process_simple_triggers.py
-python -B -OO process\process_dialogs.py
-python -B -OO process\process_postfx.py     && rem <-- just for wb
-python -B -OO process\process_global_variables_unused.py
+python -B -OO process\process_all.py
+
+:: python -B -OO process\process_init.py
+:: python -B -OO process\process_global_variables.py
+:: python -B -OO process\process_strings.py
+:: python -B -OO process\process_skills.py
+:: python -B -OO process\process_music.py
+:: python -B -OO process\process_animations.py
+:: python -B -OO process\process_meshes.py
+:: python -B -OO process\process_sounds.py
+:: python -B -OO process\process_skins.py
+:: python -B -OO process\process_map_icons.py
+:: python -B -OO process\process_factions.py
+:: python -B -OO process\process_items.py
+:: python -B -OO process\process_scenes.py
+:: python -B -OO process\process_troops.py
+:: python -B -OO process\process_particle_sys.py
+:: python -B -OO process\process_scene_props.py
+:: python -B -OO process\process_tableau_materials.py
+:: python -B -OO process\process_presentations.py
+:: python -B -OO process\process_party_tmps.py
+:: python -B -OO process\process_parties.py
+:: python -B -OO process\process_quests.py
+:: python -B -OO process\process_info_pages.py && rem <-- just for wb
+:: python -B -OO process\process_scripts.py
+:: python -B -OO process\process_mission_tmps.py
+:: python -B -OO process\process_game_menus.py
+:: python -B -OO process\process_simple_triggers.py
+:: python -B -OO process\process_dialogs.py
+:: python -B -OO process\process_postfx.py     && rem <-- just for wb
+:: python -B -OO process\process_global_variables_unused.py
 :: @del *.pyc -- not needed anymore
 
 REM count objects... (mtarini)
