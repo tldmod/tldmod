e191a691630179ba8eeb4a2651f4d49e68a43a81
Kham <kham0214@gmail.com>
2017-11-09 15:31:22 -0500

    *Add Horse Archer AI Horse Archery limit + fixes to Defend Refugee Quest + Extend Scout Camp & Defend/Raid village quests - Horse Archery has to be = or > to 4 for HA AI to work. - Changed some troops to have horse archer 4 due to the above change (Some Rhun, Rohan, and Gundabad troops).

--- ModuleSystem/module_mission_templates_TLD_wb.py
+++ ModuleSystem/module_mission_templates_TLD_wb.py
@@ -1306,6 +1306,11 @@ tld_improved_horse_archer_ai =  (0.5, 0, 0, [(eq,"$field_ai_horse_archer",1)],
             (agent_is_alive, ":agent_no"),
             (agent_is_human, ":agent_no"),
             (agent_is_non_player, ":agent_no"),
+            
+            (agent_get_troop_id, ":troop_id", ":agent_no"),
+            (store_skill_level, ":horse_archery_level", "skl_horse_archery", ":troop_id"),
+            (ge, ":horse_archery_level", 4),
+            
             (neg|agent_slot_eq, ":agent_no", 1003, 1),
             (agent_get_horse, ":horse_no", ":agent_no"),
             (assign, ":melee_weapon", -1),
