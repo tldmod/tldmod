9d3247117ebec21630e3f1f4537e938f8f53ef9b
In-Vain <joeran.balks@hotmail.de>
2020-07-07 14:27:06 +0200

    Fix savegame version script firing too early when starting a new game

--- ModuleSystem/module_map_icons.py
+++ ModuleSystem/module_map_icons.py
@@ -197,6 +197,7 @@ map_icons = map_icons_scribbler + [
       [
          # (dialog_box, "@LOLOL","@INITIALIZED morannon"),
            (call_script, "script_tld_internal_set_good_or_evil_ui"),
+		   (gt, "$savegame_version", 0), #InVain: We need this condition so the script doesn't fire before the savegame version is assigned in script_game_start
            (call_script, "script_update_savegame"),
       ]
     ),
