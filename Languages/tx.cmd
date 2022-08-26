cfda4642b366fbbebf078f99cab72b8e433cbdd9
Swyter <swyterzone@gmail.com>
2022-08-26 18:26:39 +0200

    Transifex: Partially revert 0ca05b25, which probably committed the push version of the tx.cmd translation-pulling script by mistake.

--- Languages/tx.cmd
+++ Languages/tx.cmd
@@ -7,13 +7,13 @@ luajit tx.lua convert
 
 ::push our latest strings to the web
 ::tx push -s -t -f --skip --no-interactive
-tx push -s --skip --no-interactive
+::tx push -s --skip --no-interactive
 ::tx push -s -t -f --skip --no-interactive
 ::tx push -t -l sv --skip --no-interactive
 ::tx push -t -l zh-Hant --skip --no-interactive
 
 ::pull latest translations
-::tx pull -a -f --skip --minimum-perc=40 --mode=reviewer
+tx pull -a -f --skip --minimum-perc=40 --mode=reviewer
 ::tx pull -a -f --skip --minimum-perc=0 --mode=reviewer
 
 ::revert back to mab format
@@ -21,9 +21,9 @@ luajit tx.lua revert
 
 :: the italian ui.csv file has manual additions from the M&B third-party translation project
 :: don't replace it, or entries will be lost; instead we append the result to it at the end
-::copy    it\ui.csv                 it\ui_transifex.csv           /y
-::copy /b it\ui_native_template.csv+it\ui_transifex.csv it\ui.csv /y
-::del     it\ui_transifex.csv
+copy    it\ui.csv                 it\ui_transifex.csv           /y
+copy /b it\ui_native_template.csv+it\ui_transifex.csv it\ui.csv /y
+del     it\ui_transifex.csv
 
 pause
 cls && goto :up
