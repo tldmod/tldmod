tldmod/tldmod

commit cf23afd6229f8e1b275cedfca3c43befb50f6d2e

Bring feature parity for shaders on Linux and macOS.
Implement the dozens of stochastic texturing variants. Fix a *lot* of missing or wrong gamma/linear/inverse gamma blocks that caused weird colors and darkening/whitening.
Implement the new cloud scrolling function, implement the map version of the chunky font shader.
Implement actually working overall map water flowmaps. This was implemented like five or six years ago on the HLSL version.
Implement extra HDR tonemapper range protection, stuff like super white letters causing bloomy lensflares.

Adds all the remaining/missing GLSL shader files. Fixes the borked graphics on non-Windows versions of the game.

Boy, this took like six hours of shader'ing, but at least we now have parity with the Windows version.
Lots of fixes for subtle mistakes and missing effects.

_wb/GLShaders/fs_font_outline.glsl
@@ -1,10 +1,12 @@
+#version 130
+
 uniform sampler2D diffuse_texture;
 varying vec4 outColor0;
 varying vec2 outTexCoord;
 
 /* inner and outer contours, 1.0/255 is invisibly thin <--> 0.0/0 the boldest */
 float intour( in float d, in float w ){
-    return smoothstep(0.52 - w, 0.52 + w, d);
+    return smoothstep(0.51 - w, 0.52 + w, d);
 }
 float contour( in float d, in float w ){
     return smoothstep(0.30 - w, 0.45 + w, d);
@@ -44,7 +46,7 @@ void main ()
 
     // weighted average, with 4 extra points having 0.5 weight each,
     // so 1 + 0.5*4 = 3 is the divisor
-    alpha = (alpha + 0.5 * asum) / 3.;
+    alpha = (alpha + asum/4);
     // -------
 
     float intour = intour( dist, width );
@@ -54,12 +56,12 @@ void main ()
                + intsamp( box.xw, width )
                + intsamp( box.zy, width );
 
-    intour = (intour + 0.5 * isum) / 3.;
+    intour = (intour + isum/4);
 
-    gl_FragColor = vec4
-    ( /* mix pure black and the text color using the inner contour mask.
+    gl_FragColor = clamp(vec4
+    ( /* mix the border and text colors using the inner contour mask.
          modulate the glyph's outer contour by the amount of transparency sent from the engine */
-      mix(vec3(0.0), outColor0.rgb, intour), alpha * outColor0.a
-    );
+      mix(vec3(0.0, 0.0, 0.0).rgb, outColor0.rgb, intour), alpha * outColor0.a
+    ), 0.0, 1.0);
 }
 

_wb/GLShaders/fs_font_outline_mtarini.glsl
@@ -1,3 +1,5 @@
+#version 130
+
 uniform sampler2D diffuse_texture;
 varying vec4 outColor0;
 varying vec3 outTexCoord;

_wb/GLShaders/fs_diffuse_map.glsl
@@ -1,8 +1,4 @@
 uniform sampler2D diffuse_texture;
-uniform sampler2D diffuse_texture_2;
-
-uniform float time_var;
-
 uniform vec4 vFogColor;
 uniform vec4 output_gamma_inv;
 varying vec4 Color;
@@ -11,20 +7,33 @@ varying vec4 SunLight;
 varying float Fog;
 varying vec3 ViewDir;
 varying vec3 WorldNormal;
+
+uniform sampler2D diffuse_texture_2;
+uniform float time_var;
+
+vec4 swy_scrolling_cloud_shadows(vec2 uvCoords)
+{
+  vec4 tex_sdw = texture2D(diffuse_texture_2, (uvCoords    * 0.20) + (time_var * 0.02));
+  vec4 tex_sdy = texture2D(diffuse_texture_2, (uvCoords    / 3.)   + (time_var * 0.00005));
+
+  return vec4((clamp((tex_sdw * tex_sdy), 0., 1.) * 1.).rgb , 1.);
+}
+
 void main ()
 {
-  vec4 tex_col_1;
   vec4 tmpvar_2;
-  vec4 tex_col = texture2D(diffuse_texture, Tex0);
-  vec4 tex_sdw = texture2D(diffuse_texture_2, (Tex0 * 0.2) + (time_var * 0.02));
 
-  tex_col_1.w = tex_col.w;
-  tex_col_1.xyz = pow (tex_col.xyz, vec3(2.2, 2.2, 2.2));
-  tmpvar_2 = (tex_col_1 * (tex_sdw * Color + SunLight));
+  vec4 tex_col = texture2D (diffuse_texture, Tex0);
+       tex_col = vec4(pow(tex_col.xyz, vec3(2.2)), tex_col.w);
+
+  vec4 tex_sdw = swy_scrolling_cloud_shadows(Tex0);
+
+
+  tmpvar_2 = (tex_col * (tex_sdw * Color + SunLight));
   float tmpvar_4;
   tmpvar_4 = (1.0 - clamp (dot (
     normalize(ViewDir)
-  ,
+  , 
     normalize(WorldNormal)
   ), 0.0, 1.0));
   tmpvar_2.xyz = (tmpvar_2.xyz * max (0.6, (
@@ -32,6 +41,6 @@ void main ()
    + 0.1)));
   tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
   tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
-  gl_FragColor = clamp(tmpvar_2, 0.0, 1.0);
+  gl_FragColor = clamp(tmpvar_2, 0., 1.);
 }
 

_wb/GLShaders/fs_diffuse_map_bump.glsl
@@ -1,0 +1,43 @@
+uniform sampler2D diffuse_texture;
+uniform sampler2D normal_texture;
+uniform vec4 vMaterialColor;
+uniform vec4 vSunColor;
+uniform vec4 vFogColor;
+uniform vec4 output_gamma_inv;
+varying vec4 Color;
+varying vec2 Tex0;
+varying float Fog;
+varying vec3 SunLightDir;
+varying vec3 ViewDir;
+varying vec3 WorldNormal;
+void main ()
+{
+  vec4 tex_col_1;
+  vec4 tmpvar_2;
+  vec4 tmpvar_3;
+  tmpvar_3 = texture2D (diffuse_texture, Tex0);
+  tex_col_1.w = tmpvar_3.w;
+  tex_col_1.xyz = pow (tmpvar_3.xyz, vec3(2.2, 2.2, 2.2));
+  vec4 tmpvar_4;
+  tmpvar_4.w = 1.0;
+  tmpvar_4.xyz = vSunColor.xyz;
+  tmpvar_2.xyz = (tex_col_1.xyz * (Color.xyz + (
+    (clamp (dot ((
+      (2.0 * texture2D (normal_texture, (Tex0 * 1.4)).xyz)
+     - 1.0), SunLightDir), 0.0, 1.0) * tmpvar_4)
+   * vMaterialColor).xyz));
+  tmpvar_2.w = Color.w;
+  float tmpvar_5;
+  tmpvar_5 = (1.0 - clamp (dot (
+    normalize(ViewDir)
+  , 
+    normalize(WorldNormal)
+  ), 0.0, 1.0));
+  tmpvar_2.xyz = (tmpvar_2.xyz * max (0.6, (
+    (tmpvar_5 * tmpvar_5)
+   + 0.1)));
+  tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
+  tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
+  gl_FragColor = clamp(tmpvar_2, 0., 1.);
+}
+

_wb/GLShaders/fs_diffuse_map_bump_SHDW.glsl
@@ -1,0 +1,90 @@
+uniform sampler2D diffuse_texture;
+uniform sampler2D normal_texture;
+uniform sampler2D shadowmap_texture;
+uniform vec4 vMaterialColor;
+uniform vec4 vSunColor;
+uniform vec4 vFogColor;
+uniform vec4 output_gamma_inv;
+uniform float fShadowMapNextPixel;
+uniform float fShadowMapSize;
+varying vec4 Color;
+varying vec2 Tex0;
+varying vec4 ShadowTexCoord;
+varying float Fog;
+varying vec3 SunLightDir;
+varying vec3 ViewDir;
+varying vec3 WorldNormal;
+void main ()
+{
+  vec4 tex_col_1;
+  vec4 tmpvar_2;
+  vec4 tmpvar_3;
+  tmpvar_3 = texture2D (diffuse_texture, Tex0);
+  tex_col_1.w = tmpvar_3.w;
+  tex_col_1.xyz = pow (tmpvar_3.xyz, vec3(2.2, 2.2, 2.2));
+  vec4 tmpvar_4;
+  tmpvar_4.w = 1.0;
+  tmpvar_4.xyz = vSunColor.xyz;
+  vec4 tmpvar_5;
+  tmpvar_5 = ((clamp (
+    dot (((2.0 * texture2D (normal_texture, (Tex0 * 1.4)).xyz) - 1.0), SunLightDir)
+  , 0.0, 1.0) * tmpvar_4) * vMaterialColor);
+  float sun_amount_6;
+  sun_amount_6 = 0.0;
+  vec2 tmpvar_7;
+  tmpvar_7 = fract((ShadowTexCoord.xy * fShadowMapSize));
+  vec4 tmpvar_8;
+  tmpvar_8 = texture2D (shadowmap_texture, ShadowTexCoord.xy);
+  float tmpvar_9;
+  if ((tmpvar_8.x < ShadowTexCoord.z)) {
+    tmpvar_9 = 0.0;
+  } else {
+    tmpvar_9 = 1.0;
+  };
+  vec2 tmpvar_10;
+  tmpvar_10.y = 0.0;
+  tmpvar_10.x = fShadowMapNextPixel;
+  vec4 tmpvar_11;
+  tmpvar_11 = texture2D (shadowmap_texture, (ShadowTexCoord.xy + tmpvar_10));
+  float tmpvar_12;
+  if ((tmpvar_11.x < ShadowTexCoord.z)) {
+    tmpvar_12 = 0.0;
+  } else {
+    tmpvar_12 = 1.0;
+  };
+  vec2 tmpvar_13;
+  tmpvar_13.x = 0.0;
+  tmpvar_13.y = fShadowMapNextPixel;
+  vec4 tmpvar_14;
+  tmpvar_14 = texture2D (shadowmap_texture, (ShadowTexCoord.xy + tmpvar_13));
+  float tmpvar_15;
+  if ((tmpvar_14.x < ShadowTexCoord.z)) {
+    tmpvar_15 = 0.0;
+  } else {
+    tmpvar_15 = 1.0;
+  };
+  vec4 tmpvar_16;
+  tmpvar_16 = texture2D (shadowmap_texture, (ShadowTexCoord.xy + vec2(fShadowMapNextPixel)));
+  float tmpvar_17;
+  if ((tmpvar_16.x < ShadowTexCoord.z)) {
+    tmpvar_17 = 0.0;
+  } else {
+    tmpvar_17 = 1.0;
+  };
+  sun_amount_6 = clamp (mix (mix (tmpvar_9, tmpvar_12, tmpvar_7.x), mix (tmpvar_15, tmpvar_17, tmpvar_7.x), tmpvar_7.y), 0.0, 1.0);
+  tmpvar_2.xyz = (tex_col_1.xyz * (Color.xyz + (tmpvar_5.xyz * sun_amount_6)));
+  tmpvar_2.w = Color.w;
+  float tmpvar_18;
+  tmpvar_18 = (1.0 - clamp (dot (
+    normalize(ViewDir)
+  , 
+    normalize(WorldNormal)
+  ), 0.0, 1.0));
+  tmpvar_2.xyz = (tmpvar_2.xyz * max (0.6, (
+    (tmpvar_18 * tmpvar_18)
+   + 0.1)));
+  tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
+  tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
+  gl_FragColor = clamp(tmpvar_2, 0., 1.);
+}
+

_wb/GLShaders/fs_diffuse_map_swy_stochastic.glsl
@@ -1,0 +1,134 @@
+#version 130
+
+uniform sampler2D diffuse_texture;
+uniform vec4 vFogColor;
+uniform vec4 output_gamma_inv;
+varying vec4 Color;
+varying vec2 Tex0;
+varying vec4 SunLight;
+varying float Fog;
+varying vec3 ViewDir;
+varying vec3 WorldNormal;
+
+uniform sampler2D diffuse_texture_2;
+uniform float time_var;
+
+/* swy: technique lifted from «Procedural Stochastic Textures by Tiling and Blending», created by Thomas Deliot and Eric Heitz (turned the GLSL into HLSL)
+        https://drive.google.com/file/d/1QecekuuyWgw68HU9tg6ENfrCTCVIjm6l/view */
+vec2 hash(ivec2 p)
+{
+  return fract(sin(mat2(127.1, 311.7, 269.5, 183.3) * vec2(p)) * 43758.5453);
+}
+
+// Compute local triangle barycentric coordinates and vertex IDs
+void TriangleGrid(vec2 uv,
+                  out float w1, out float w2, out float w3,
+                  out ivec2 vertex1, out ivec2 vertex2, out ivec2 vertex3
+)
+{
+  // Scaling of the input
+  uv *= 3.464; /* 2 * sqrt(3) */
+  
+  // Skew input space into simplex triangle grid
+  const mat2 gridToSkewedGrid = mat2(1.0, 0.0, -0.57735027 /* tan(-30◦) */, 1.15470054 /* Saw-tooth waveform: 2 / sqrt(3) */);
+  vec2 skewedCoord = gridToSkewedGrid * uv;
+  
+  // Compute local triangle vertex IDs and local barycentric coordinates
+  ivec2 baseId = ivec2(floor(skewedCoord)   );
+  vec3  temp   =  vec3(fract(skewedCoord), 0);
+        temp.z = 1.0 - temp.x - temp.y;
+  
+  if (temp.z > 0.0)
+  {
+    w1 =       temp.z;
+    w2 =       temp.y;
+    w3 =       temp.x;
+    vertex1 = baseId;
+    vertex2 = baseId + ivec2(0, 1);
+    vertex3 = baseId + ivec2(1, 0);
+  }
+  else
+  {
+    w1 =     - temp.z;
+    w2 = 1.0 - temp.y;
+    w3 = 1.0 - temp.x;
+    vertex1 = baseId + ivec2(1, 1);
+    vertex2 = baseId + ivec2(1, 0);
+    vertex3 = baseId + ivec2(0, 1);
+  }
+}
+
+vec4 stochasticTex2D(sampler2D texSampler, vec2 uvCoords, bool gammaCorrect) /* swy: use gamma-correction only for diffuse/real-color textures, not for things like normal or specular maps */
+{
+    float w1, w2, w3; ivec2 vertex1, vertex2, vertex3;
+    TriangleGrid(uvCoords, w1, w2, w3, vertex1, vertex2, vertex3);
+    
+    /* swy: each triangle offsets/nudges the base UV coordinates in a different direction;
+            we use the ID as a hash seed, so that for that index the random offset is always the same; it's permanent */
+    vec2 uvA = uvCoords + hash(vertex1);
+    vec2 uvB = uvCoords + hash(vertex2);
+    vec2 uvC = uvCoords + hash(vertex3);
+    
+    /* swy: manually grab the screen-space derivatives to do correct mipmapping without shimmering, and anisotropic filtering */
+    vec2 duvdx = dFdx(uvCoords);
+    vec2 duvdy = dFdy(uvCoords);
+    
+    /* swy: sample the texels of every overlapping triangle under this point; there are always three of them
+            note: it's important do the gamma correction right at the start; if we blend and then gamma-correct the result, it will look wrong */
+    vec4 texA = textureGrad(texSampler, uvA, duvdx, duvdy); if (gammaCorrect) texA.rgb = pow(texA.rgb, vec3(2.2));
+    vec4 texB = textureGrad(texSampler, uvB, duvdx, duvdy); if (gammaCorrect) texB.rgb = pow(texB.rgb, vec3(2.2));
+    vec4 texC = textureGrad(texSampler, uvC, duvdx, duvdy); if (gammaCorrect) texC.rgb = pow(texC.rgb, vec3(2.2));
+    
+    /* swy: use a simple linear blend; no fancy histogram-preserving texture preprocessing here;
+            we could also use some kind of height blending, by exploiting the .z channels of
+            normalmaps or the albedo itself with some RGB tweaking */
+    return (w1 * texA) + (w2 * texB) + (w3 * texC);
+}
+
+
+vec4 stochasticTex2DWithDistFadeOut(sampler2D texSampler, vec2 uvCoords, bool gammaCorrect)
+{
+    vec4 stocColor = stochasticTex2D(texSampler, uvCoords, gammaCorrect);
+    vec4 origColor =       texture2D(texSampler, uvCoords);
+    
+    if (gammaCorrect)
+        origColor.rgb = pow(origColor.rgb, vec3(2.2));
+    
+    return stocColor;//lerp(origColor, stocColor, .5);
+}
+
+/* swy: -- */
+
+vec4 swy_scrolling_cloud_shadows(vec2 uvCoords)
+{
+  vec4 tex_sdw = texture2D(diffuse_texture_2, (uvCoords    * 0.20) + (time_var * 0.02));
+  vec4 tex_sdy = texture2D(diffuse_texture_2, (uvCoords    / 3.)   + (time_var * 0.00005));
+
+  return vec4((clamp((tex_sdw * tex_sdy), 0., 1.) * 1.).rgb , 1.);
+}
+
+void main ()
+{
+  vec4 tmpvar_2;
+
+  vec4 tex_col = stochasticTex2D(diffuse_texture, Tex0, false);
+       tex_col = vec4(pow(tex_col.xyz, vec3(2.2)), tex_col.w);
+
+  vec4 tex_sdw = swy_scrolling_cloud_shadows(Tex0);
+
+
+  tmpvar_2 = (tex_col * (tex_sdw * Color + SunLight));
+  float tmpvar_4;
+  tmpvar_4 = (1.0 - clamp (dot (
+    normalize(ViewDir)
+  , 
+    normalize(WorldNormal)
+  ), 0.0, 1.0));
+  tmpvar_2.xyz = (tmpvar_2.xyz * max (0.6, (
+    (tmpvar_4 * tmpvar_4)
+   + 0.1)));
+  tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
+  tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
+  gl_FragColor = clamp(tmpvar_2, 0., 1.);
+}
+

_wb/GLShaders/fs_diffuse_map_swy_stochastic_SHDW.glsl
@@ -1,0 +1,178 @@
+#version 130
+
+uniform sampler2D diffuse_texture;
+uniform sampler2D shadowmap_texture;
+uniform vec4 vFogColor;
+uniform vec4 output_gamma_inv;
+uniform float fShadowMapNextPixel;
+uniform float fShadowMapSize;
+varying vec4 Color;
+varying vec2 Tex0;
+varying vec4 SunLight;
+varying vec4 ShadowTexCoord;
+varying float Fog;
+varying vec3 ViewDir;
+varying vec3 WorldNormal;
+
+uniform sampler2D diffuse_texture_2;
+uniform float time_var;
+
+/* swy: technique lifted from «Procedural Stochastic Textures by Tiling and Blending», created by Thomas Deliot and Eric Heitz (turned the GLSL into HLSL)
+        https://drive.google.com/file/d/1QecekuuyWgw68HU9tg6ENfrCTCVIjm6l/view */
+vec2 hash(ivec2 p)
+{
+  return fract(sin(mat2(127.1, 311.7, 269.5, 183.3) * vec2(p)) * 43758.5453);
+}
+
+// Compute local triangle barycentric coordinates and vertex IDs
+void TriangleGrid(vec2 uv,
+                  out float w1, out float w2, out float w3,
+                  out ivec2 vertex1, out ivec2 vertex2, out ivec2 vertex3
+)
+{
+  // Scaling of the input
+  uv *= 3.464; /* 2 * sqrt(3) */
+  
+  // Skew input space into simplex triangle grid
+  const mat2 gridToSkewedGrid = mat2(1.0, 0.0, -0.57735027 /* tan(-30◦) */, 1.15470054 /* Saw-tooth waveform: 2 / sqrt(3) */);
+  vec2 skewedCoord = gridToSkewedGrid * uv;
+  
+  // Compute local triangle vertex IDs and local barycentric coordinates
+  ivec2 baseId = ivec2(floor(skewedCoord)   );
+  vec3  temp   =  vec3(fract(skewedCoord), 0);
+        temp.z = 1.0 - temp.x - temp.y;
+  
+  if (temp.z > 0.0)
+  {
+    w1 =       temp.z;
+    w2 =       temp.y;
+    w3 =       temp.x;
+    vertex1 = baseId;
+    vertex2 = baseId + ivec2(0, 1);
+    vertex3 = baseId + ivec2(1, 0);
+  }
+  else
+  {
+    w1 =     - temp.z;
+    w2 = 1.0 - temp.y;
+    w3 = 1.0 - temp.x;
+    vertex1 = baseId + ivec2(1, 1);
+    vertex2 = baseId + ivec2(1, 0);
+    vertex3 = baseId + ivec2(0, 1);
+  }
+}
+
+vec4 stochasticTex2D(sampler2D texSampler, vec2 uvCoords, bool gammaCorrect) /* swy: use gamma-correction only for diffuse/real-color textures, not for things like normal or specular maps */
+{
+    float w1, w2, w3; ivec2 vertex1, vertex2, vertex3;
+    TriangleGrid(uvCoords, w1, w2, w3, vertex1, vertex2, vertex3);
+    
+    /* swy: each triangle offsets/nudges the base UV coordinates in a different direction;
+            we use the ID as a hash seed, so that for that index the random offset is always the same; it's permanent */
+    vec2 uvA = uvCoords + hash(vertex1);
+    vec2 uvB = uvCoords + hash(vertex2);
+    vec2 uvC = uvCoords + hash(vertex3);
+    
+    /* swy: manually grab the screen-space derivatives to do correct mipmapping without shimmering, and anisotropic filtering */
+    vec2 duvdx = dFdx(uvCoords);
+    vec2 duvdy = dFdy(uvCoords);
+    
+    /* swy: sample the texels of every overlapping triangle under this point; there are always three of them
+            note: it's important do the gamma correction right at the start; if we blend and then gamma-correct the result, it will look wrong */
+    vec4 texA = textureGrad(texSampler, uvA, duvdx, duvdy); if (gammaCorrect) texA.rgb = pow(texA.rgb, vec3(2.2));
+    vec4 texB = textureGrad(texSampler, uvB, duvdx, duvdy); if (gammaCorrect) texB.rgb = pow(texB.rgb, vec3(2.2));
+    vec4 texC = textureGrad(texSampler, uvC, duvdx, duvdy); if (gammaCorrect) texC.rgb = pow(texC.rgb, vec3(2.2));
+    
+    /* swy: use a simple linear blend; no fancy histogram-preserving texture preprocessing here;
+            we could also use some kind of height blending, by exploiting the .z channels of
+            normalmaps or the albedo itself with some RGB tweaking */
+    return (w1 * texA) + (w2 * texB) + (w3 * texC);
+}
+
+
+vec4 stochasticTex2DWithDistFadeOut(sampler2D texSampler, vec2 uvCoords, bool gammaCorrect)
+{
+    vec4 stocColor = stochasticTex2D(texSampler, uvCoords, gammaCorrect);
+    vec4 origColor =       texture2D(texSampler, uvCoords);
+    
+    if (gammaCorrect)
+        origColor.rgb = pow(origColor.rgb, vec3(2.2));
+    
+    return stocColor;//lerp(origColor, stocColor, .5);
+}
+
+/* swy: -- */
+
+vec4 swy_scrolling_cloud_shadows(vec2 uvCoords)
+{
+  vec4 tex_sdw = texture2D(diffuse_texture_2, (uvCoords    * 0.20) + (time_var * 0.02));
+  vec4 tex_sdy = texture2D(diffuse_texture_2, (uvCoords    / 3.)   + (time_var * 0.00005));
+
+  return vec4((clamp((tex_sdw * tex_sdy), 0., 1.) * 1.).rgb , 1.);
+}
+
+void main ()
+{
+  vec4 tmpvar_2;
+
+  vec4 tex_col = stochasticTex2D(diffuse_texture, Tex0, true);
+  vec4 tex_sdw = swy_scrolling_cloud_shadows(Tex0);
+  
+  float sun_amount_4;
+  sun_amount_4 = 0.0;
+  vec2 tmpvar_5;
+  tmpvar_5 = fract((ShadowTexCoord.xy * fShadowMapSize));
+  vec4 tmpvar_6;
+  tmpvar_6 = texture2D (shadowmap_texture, ShadowTexCoord.xy);
+  float tmpvar_7;
+  if ((tmpvar_6.x < ShadowTexCoord.z)) {
+    tmpvar_7 = 0.0;
+  } else {
+    tmpvar_7 = 1.0;
+  };
+  vec2 tmpvar_8;
+  tmpvar_8.y = 0.0;
+  tmpvar_8.x = fShadowMapNextPixel;
+  vec4 tmpvar_9;
+  tmpvar_9 = texture2D (shadowmap_texture, (ShadowTexCoord.xy + tmpvar_8));
+  float tmpvar_10;
+  if ((tmpvar_9.x < ShadowTexCoord.z)) {
+    tmpvar_10 = 0.0;
+  } else {
+    tmpvar_10 = 1.0;
+  };
+  vec2 tmpvar_11;
+  tmpvar_11.x = 0.0;
+  tmpvar_11.y = fShadowMapNextPixel;
+  vec4 tmpvar_12;
+  tmpvar_12 = texture2D (shadowmap_texture, (ShadowTexCoord.xy + tmpvar_11));
+  float tmpvar_13;
+  if ((tmpvar_12.x < ShadowTexCoord.z)) {
+    tmpvar_13 = 0.0;
+  } else {
+    tmpvar_13 = 1.0;
+  };
+  vec4 tmpvar_14;
+  tmpvar_14 = texture2D (shadowmap_texture, (ShadowTexCoord.xy + vec2(fShadowMapNextPixel)));
+  float tmpvar_15;
+  if ((tmpvar_14.x < ShadowTexCoord.z)) {
+    tmpvar_15 = 0.0;
+  } else {
+    tmpvar_15 = 1.0;
+  };
+  sun_amount_4 = clamp (mix (mix (tmpvar_7, tmpvar_10, tmpvar_5.x), mix (tmpvar_13, tmpvar_15, tmpvar_5.x), tmpvar_5.y), 0.0, 1.0);
+  tmpvar_2 = (tex_col * (tex_sdw * Color + (SunLight * sun_amount_4)));
+  float tmpvar_16;
+  tmpvar_16 = (1.0 - clamp (dot (
+    normalize(ViewDir)
+  , 
+    normalize(WorldNormal)
+  ), 0.0, 1.0));
+  tmpvar_2.xyz = (tmpvar_2.xyz * max (0.6, (
+    (tmpvar_16 * tmpvar_16)
+   + 0.1)));
+  tmpvar_2.xyz = pow (tmpvar_2.xyz, output_gamma_inv.xyz);
+  tmpvar_2.xyz = mix (vFogColor.xyz, tmpvar_2.xyz, Fog);
+  gl_FragColor = clamp(tmpvar_2, 0., 1.);
+}
+

_wb/GLShaders/fs_dot3_multitex_swy_stochastic.glsl
@@ -1,0 +1,155 @@
+#version 130
+
+uniform sampler2D diffuse_texture;
+uniform sampler2D diffuse_texture_2;
+uniform sampler2D normal_texture;
+uniform vec4 vSunColor;
+uniform vec4 vAmbientColor;
+uniform vec4 vSkyLightColor;
+uniform vec4 vPointLightColor;
+uniform vec4 vFogColor;
+uniform vec4 output_gamma_inv;
+varying vec4 VertexColor;
+varying vec2 Tex0;
+varying vec3 SunLightDir;
+varying vec3 SkyLightDir;
+varying vec4 PointLightDir;
+varying float Fog;
+varying vec3 ViewDir;
+varying vec3 WorldNormal;
+
+uniform float time_var;
+
+/* swy: technique lifted from «Procedural Stochastic Textures by Tiling and Blending», created by Thomas Deliot and Eric Heitz (turned the GLSL into HLSL)
+        https://drive.google.com/file/d/1QecekuuyWgw68HU9tg6ENfrCTCVIjm6l/view */
+vec2 hash(ivec2 p)
+{
+  return fract(sin(mat2(127.1, 311.7, 269.5, 183.3) * vec2(p)) * 43758.5453);
+}
+
+// Compute local triangle barycentric coordinates and vertex IDs
+void TriangleGrid(vec2 uv,
+                  out float w1, out float w2, out float w3,
+                  out ivec2 vertex1, out ivec2 vertex2, out ivec2 vertex3
+)
+{
+  // Scaling of the input
+  uv *= 3.464; /* 2 * sqrt(3) */
+  
+  // Skew input space into simplex triangle grid
+  const mat2 gridToSkewedGrid = mat2(1.0, 0.0, -0.57735027 /* tan(-30◦) */, 1.15470054 /* Saw-tooth waveform: 2 / sqrt(3) */);
+  vec2 skewedCoord = gridToSkewedGrid * uv;
+  
+  // Compute local triangle vertex IDs and local barycentric coordinates
+  ivec2 baseId = ivec2(floor(skewedCoord)   );
+  vec3  temp   =  vec3(fract(skewedCoord), 0);
+        temp.z = 1.0 - temp.x - temp.y;
+  
+  if (temp.z > 0.0)
+  {
+    w1 =       temp.z;
+    w2 =       temp.y;
+    w3 =       temp.x;
+    vertex1 = baseId;
+    vertex2 = baseId + ivec2(0, 1);
+    vertex3 = baseId + ivec2(1, 0);
+  }
+  else
+  {
+    w1 =     - temp.z;
+    w2 = 1.0 - temp.y;
+    w3 = 1.0 - temp.x;
+    vertex1 = baseId + ivec2(1, 1);
+    vertex2 = baseId + ivec2(1, 0);
+    vertex3 = baseId + ivec2(0, 1);
+  }
+}
+
+vec4 stochasticTex2D(sampler2D texSampler, vec2 uvCoords, bool gammaCorrect) /* swy: use gamma-correction only for diffuse/real-color textures, not for things like normal or specular maps */
+{
+    float w1, w2, w3; ivec2 vertex1, vertex2, vertex3;
+    TriangleGrid(uvCoords, w1, w2, w3, vertex1, vertex2, vertex3);
+    
+    /* swy: each triangle offsets/nudges the base UV coordinates in a different direction;
+            we use the ID as a hash seed, so that for that index the random offset is always the same; it's permanent */
+    vec2 uvA = uvCoords + hash(vertex1);
+    vec2 uvB = uvCoords + hash(vertex2);
+    vec2 uvC = uvCoords + hash(vertex3);
+    
+    /* swy: manually grab the screen-space derivatives to do correct mipmapping without shimmering, and anisotropic filtering */
+    vec2 duvdx = dFdx(uvCoords);
+    vec2 duvdy = dFdy(uvCoords);
+    
+    /* swy: sample the texels of every overlapping triangle under this point; there are always three of them
+            note: it's important do the gamma correction right at the start; if we blend and then gamma-correct the result, it will look wrong */
+    vec4 texA = textureGrad(texSampler, uvA, duvdx, duvdy); if (gammaCorrect) texA.rgb = pow(texA.rgb, vec3(2.2));
+    vec4 texB = textureGrad(texSampler, uvB, duvdx, duvdy); if (gammaCorrect) texB.rgb = pow(texB.rgb, vec3(2.2));
+    vec4 texC = textureGrad(texSampler, uvC, duvdx, duvdy); if (gammaCorrect) texC.rgb = pow(texC.rgb, vec3(2.2));
+    
+    /* swy: use a simple linear blend; no fancy histogram-preserving texture preprocessing here;
+            we could also use some kind of height blending, by exploiting the .z channels of
+            normalmaps or the albedo itself with some RGB tweaking */
+    return (w1 * texA) + (w2 * texB) + (w3 * texC);
+}
+
+
+vec4 stochasticTex2DWithDistFadeOut(sampler2D texSampler, vec2 uvCoords, bool gammaCorrect)
+{
+    vec4 stocColor = stochasticTex2D(texSampler, uvCoords, gammaCorrect);
+    vec4 origColor =       texture2D(texSampler, uvCoords);
+    
+    if (gammaCorrect)
+        origColor.rgb = pow(origColor.rgb, vec3(2.2));
+    
+    return stocColor;//lerp(origColor, stocColor, .5);
+}
+
+/* swy: -- */
+
+
+void main ()
+{
+  vec4 multi_tex_col_1;
+  vec4 total_light_2;
+  vec4 tmpvar_3;
+  vec4 tmpvar_4;
+  tmpvar_4 = stochasticTex2D(diffuse_texture, Tex0, true);
+  multi_tex_col_1.w = tmpvar_4.w;
+  multi_tex_col_1.xyz = (tmpvar_4.xyz * (1.0 - VertexColor.w));
+  multi_tex_col_1.xyz = (multi_tex_col_1.xyz + (stochasticTex2D(diffuse_texture_2, (Tex0 * 1.237), true).xyz * VertexColor.w));
+  vec3 tmpvar_5;
+  tmpvar_5 = ((2.0 * stochasticTex2D(normal_texture, Tex0, false).xyz) - 1.0);
+  vec4 tmpvar_6;
+  tmpvar_6.w = 1.0;
+  tmpvar_6.xyz = vSunColor.xyz;
+  total_light_2 = (vAmbientColor + (clamp (
+    dot (SunLightDir, tmpvar_5)
+  , 0.0, 1.0) * tmpvar_6));
+  total_light_2 = (total_light_2 + (clamp (
+    dot (SkyLightDir, tmpvar_5)
+  , 0.0, 1.0) * vSkyLightColor));
+  vec4 tmpvar_7;
+  tmpvar_7.w = 1.0;
+  tmpvar_7.xyz = vPointLightColor.xyz;
+  total_light_2 = (total_light_2 + (clamp (
+    dot (PointLightDir.xyz, tmpvar_5)
+  , 0.0, 1.0) * tmpvar_7));
+  tmpvar_3.xyz = total_light_2.xyz;
+  tmpvar_3.w = 1.0;
+  tmpvar_3 = (tmpvar_3 * multi_tex_col_1);
+  tmpvar_3.xyz = (tmpvar_3.xyz * VertexColor.xyz);
+  tmpvar_3.w = (tmpvar_3.w * PointLightDir.w);
+  float tmpvar_8;
+  tmpvar_8 = (1.0 - clamp (dot (
+    normalize(ViewDir)
+  , 
+    normalize(WorldNormal)
+  ), 0.0, 1.0));
+  tmpvar_3.xyz = (tmpvar_3.xyz * max (0.6, (
+    (tmpvar_8 * tmpvar_8)
+   + 0.1)));
+  tmpvar_3.xyz = pow (tmpvar_3.xyz, output_gamma_inv.xyz);
+  tmpvar_3.xyz = mix (vFogColor.xyz, tmpvar_3.xyz, Fog);
+  gl_FragColor = tmpvar_3;
+}
+

commit 5f8d247a70f87e9fe734c45a4b950c7eb19f021a

Fix broken fonts on macOS, I broke it in cf23afd, last working version was r3613. The macOS GLSL parser does not like #version 130 at all. Don't divide by integers; use / 4.0 instead of just 4. I still need to make a workaround for textureGrad() while still keeping it working on Linux, which does like #version 130. Eugh.

_wb/GLShaders/fs_font_outline.glsl
@@ -1,4 +1,4 @@
-#version 130
+/* swy: don't set version 130 as macOS doesn't support it */
 
 uniform sampler2D diffuse_texture;
 varying vec4 outColor0;
@@ -46,7 +46,7 @@ void main ()
 
     // weighted average, with 4 extra points having 0.5 weight each,
     // so 1 + 0.5*4 = 3 is the divisor
-    alpha = (alpha + asum/4);
+    alpha = (alpha + asum/4.0);
     // -------
 
     float intour = intour( dist, width );
@@ -56,7 +56,7 @@ void main ()
                + intsamp( box.xw, width )
                + intsamp( box.zy, width );
 
-    intour = (intour + isum/4);
+    intour = (intour + isum/4.0);
 
     gl_FragColor = clamp(vec4
     ( /* mix the border and text colors using the inner contour mask.

_wb/GLShaders/fs_font_outline_mtarini.glsl
@@ -1,4 +1,4 @@
-#version 130
+/* swy: don't set version 130 as macOS doesn't support it */
 
 uniform sampler2D diffuse_texture;
 varying vec4 outColor0;