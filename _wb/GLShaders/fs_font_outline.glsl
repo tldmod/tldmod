849db8d704f15b5e5f70dd8ade54d3056018a15a
Swyter <swyterzone@gmail.com>
2022-11-23 00:39:00 +0100

    Let people change the custom font on macOS and Linux; add a GLSL variant with the custom SDF technique removed.

--- _wb/GLShaders/fs_font_outline.glsl
+++ _wb/GLShaders/fs_font_outline.glsl
@@ -1,67 +1,18 @@
-#version 130
+/* swy: don't set version 130 as macOS doesn't support it */
 
 uniform sampler2D diffuse_texture;
 varying vec4 outColor0;
 varying vec2 outTexCoord;
 
-/* inner and outer contours, 1.0/255 is invisibly thin <--> 0.0/0 the boldest */
-float intour( in float d, in float w ){
-    return smoothstep(0.51 - w, 0.52 + w, d);
-}
-float contour( in float d, in float w ){
-    return smoothstep(0.30 - w, 0.45 + w, d);
-}
-
-/* just simple macros, could be a bit less messy */
-float samp( in vec2 uv, float w ){
-    return contour( 1.0 - texture2D(diffuse_texture,uv).r, w );
-}
-float intsamp( in vec2 uv, float w ){
-    return intour( 1.0 - texture2D(diffuse_texture,uv).r, w );
-}
-
-
 void main ()
 {
- /* supersampled signed font distance technique (with partial derivatives) by /u/glacialthinker on reddit
-    https://www.reddit.com/r/gamedev/comments/2879jd/just_found_out_about_signed_distance_field_text/cicatot/ */
-
     vec2 uv = outTexCoord.xy;
+    vec4 sample = texture2D(diffuse_texture, uv);
+    
+    vec4 RGBColor = outColor0;
+    RGBColor.a    = (1.0 - sample.r) + sample.a;
+    RGBColor.rgb *= 0.6 * sample.a + 0.4;
 
-    float dist = 1.0 - texture2D( diffuse_texture, uv ).r;
-    float width = fwidth(dist);
-
-    float alpha = contour( dist, width );
-
-    // ------- (comment this block out to get your original behavior)
-    // Supersample, 4 extra points
-    const float dscale = 0.354; // half of 1/sqrt2; you can play with this
-    vec2 duv = dscale * (dFdx(uv) + dFdy(uv));
-    vec4 box = vec4(uv-duv, uv+duv);
-
-    float asum = samp( box.xy, width )
-               + samp( box.zw, width )
-               + samp( box.xw, width )
-               + samp( box.zy, width );
-
-    // weighted average, with 4 extra points having 0.5 weight each,
-    // so 1 + 0.5*4 = 3 is the divisor
-    alpha = (alpha + asum/4);
-    // -------
-
-    float intour = intour( dist, width );
-
-    float isum = intsamp( box.xy, width )
-               + intsamp( box.zw, width )
-               + intsamp( box.xw, width )
-               + intsamp( box.zy, width );
-
-    intour = (intour + isum/4);
-
-    gl_FragColor = clamp(vec4
-    ( /* mix the border and text colors using the inner contour mask.
-         modulate the glyph's outer contour by the amount of transparency sent from the engine */
-      mix(vec3(0.0, 0.0, 0.0).rgb, outColor0.rgb, intour), alpha * outColor0.a
-    ), 0.0, 1.0);
+    gl_FragColor = RGBColor;
 }
 
--- _wb/GLShaders/fs_font_outline_mtarini.glsl
+++ _wb/GLShaders/fs_font_outline_mtarini.glsl
@@ -1,71 +1,25 @@
-#version 130
+/* swy: don't set version 130 as macOS doesn't support it */
 
 uniform sampler2D diffuse_texture;
 varying vec4 outColor0;
 varying vec3 outTexCoord;
 
-/* inner and outer contours, 1.0/255 is invisibly thin <--> 0.0/0 the boldest */
-float intour( in float d, in float w ){
-    return smoothstep(0.48 - w, 0.48 + w, d);
-}
-float contour( in float d, in float w ){
-    return smoothstep(0.30 - w, 0.49 + w, d);
-}
-
-/* just simple macros, could be a bit less messy */
-float samp( in vec2 uv, float w ){
-    return contour( 1.0 - texture2D(diffuse_texture,uv).r, w );
-}
-float intsamp( in vec2 uv, float w ){
-    return intour( 1.0 - texture2D(diffuse_texture,uv).r, w );
-}
-
-
 void main ()
 {
- /* supersampled signed font distance technique (with partial derivatives) by /u/glacialthinker on reddit
-    https://www.reddit.com/r/gamedev/comments/2879jd/just_found_out_about_signed_distance_field_text/cicatot/ */
-
     vec2 uv = outTexCoord.xy;
-
-    float dist = 1.0 - texture2D( diffuse_texture, uv ).r;
-    float width = fwidth(dist);
-
-    float alpha = contour( dist, width );
-
-    // ------- (comment this block out to get your original behavior)
-    // Supersample, 4 extra points
-    const float dscale = 0.354; // half of 1/sqrt2; you can play with this
-    vec2 duv = dscale * (dFdx(uv) + dFdy(uv));
-    vec4 box = vec4(uv-duv, uv+duv);
-
-    float asum = samp( box.xy, width )
-               + samp( box.zw, width )
-               + samp( box.xw, width )
-               + samp( box.zy, width );
-
-    // weighted average, with 4 extra points having 0.5 weight each,
-    // so 1 + 0.5*4 = 3 is the divisor
-    alpha = (alpha + 0.5 * asum) / 3.;
-    // -------
-
-    float intour = intour( dist, width );
-
-    float isum = intsamp( box.xy, width )
-               + intsamp( box.zw, width )
-               + intsamp( box.xw, width )
-               + intsamp( box.zy, width );
-
-    intour = (intour + 0.5 * isum) / 3.;
+    vec4 sample = texture2D(diffuse_texture, uv);
 
     /* this basically is a boolean variable from the vertex shader that makes
        the outline/shadow either black or white, depending on the font color. */
     float bordColor = outTexCoord.z;
+    float bord = clamp((1.0 - sample.r) * 2.0, 0., 1.);
 
-    gl_FragColor = vec4
-    ( /* mix the border and text colors using the inner contour mask.
-         modulate the glyph's outer contour by the amount of transparency sent from the engine */
-      mix(vec3(bordColor), outColor0.rgb, intour), alpha * outColor0.a
-    );
+    vec4 RGBColor; RGBColor.a = outColor0.a * (bord * (0.40 + 0.30 * (1.0 - sample.g)) + sample.a);
+
+    float isB = (1.0 - sample.a) * (1.0 - 0.);
+
+    RGBColor.rgb = outColor0.rgb * (1.0 - isB) + vec3(bordColor) * (isB);
+
+    gl_FragColor = RGBColor;
 }
 
