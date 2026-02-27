d060d9118faba23baad5338efdd24a823b7e9c39
Swyter <swyterzone@gmail.com>
2026-02-27 01:40:29 +0100

    Improve my `_setupsymlinks.bat` script from 2011 so that it knows which files are symbolic shortcuts and recreates them, leaving the normal files alone.
    Make sure they still point to the same file, should make the whole system less brittle. Make the script accept unquoted paths with spaces at the end.
    
    Clarify and demystify usage a little bit.

--- _wb/_setupsymlinks.bat
+++ _wb/_setupsymlinks.bat
@@ -2,10 +2,25 @@
 :: echo. > _setupsymlinks.log
 
 :createlink
- if not exist %2 (
+ if not exist "%2" (
 	echo [/] Creating junction for ^<%2^>...
  ) else (
 	echo [!] File already there ^<%2^>...
+	
+	:: swy: running 'fsutil hardlink list <file>' shows a the list of paths that share the same file
+	::      we can count the lines that the command outputs and if they are more than 1 then that
+	::      probably means this file is a symlink. at least under the _wb folder.
+	for /f "usebackq" %%b in (`fsutil hardlink list "%2" ^| find "" /v /c`) do (
+		
+		:: swy: 1 normal file: ignore. 2 or more: symlink. get rid of it so that we can recreate it afterwards
+		::      we want to recreate links because files and folders may have moved around, and the
+		::      links may no longer point to the right file, as the mod changes
+		if %%b geq 2 (
+			echo  [i] This is a junction; line count is %%b. deleting first:
+			echo       %~f2
+			del "%2"
+		)
+	)
  )
  
  if %1 neq "H" (
@@ -49,8 +64,8 @@ echo     so don't create a module folder manually under WB, it's automatic!
 echo. 
 echo     Close to skip, in case it^'s already done.
 echo. 
-set /p wbpath=Type something like [c:\wb\modules\tld] : 
-mklink /J %wbpath% .
+set /p wbpath=Type something like [C:\Program Files (x86)\Mount^&Blade Warband\Modules\tld] : 
+mklink /J "%wbpath%" .
 
 echo. && echo --- && echo It^'s done, you can close this window now... How cool is that?
                      echo Oh, and compile Warband to generate the text files! && pause>nul
